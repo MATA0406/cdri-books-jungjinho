911ce9696d2660d76b3c065caf1b9b7e326ba686
MATA0406 <jinho_46@naver.com>
2025-06-17 17:14:42 +0900

    폰트 로컬로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { Global } from '@emotion/react';
 import { globalStyle } from './styles/global';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// Noto Sans KR 폰트 import
+import '@fontsource/noto-sans-kr/300.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/500.css';
+import '@fontsource/noto-sans-kr/600.css';
+import '@fontsource/noto-sans-kr/700.css';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
